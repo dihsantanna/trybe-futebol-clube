fc63d4ba4039a604d6a88d03a5df6c6bb7a56582
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-03-23 14:05:42 -0300

    refatora classes HandlerToken e LoginService

--- app/backend/src/database/services/login.ts
+++ app/backend/src/database/services/login.ts
@@ -10,13 +10,14 @@ import msgs from '../../environments/msgsError';
 export default class LoginService {
   constructor(
     readonly loginRepository: LoginRepository,
+    readonly handlerToken = HandlerToken,
   ) {}
 
   login = async ({ email, password }: ILogin) => {
     const user = await this.loginRepository.findUser(email) as IUser;
     if (!!user && await bcrypt.compare(password, user.password)) {
       const { id, username, role } = user;
-      const token = await HandlerToken.generate(user);
+      const token = await this.handlerToken.generate(user);
       return {
         code: code.OK,
         result: {
@@ -29,7 +30,7 @@ export default class LoginService {
   };
 
   validate = async (token: string) => {
-    const payload = await HandlerToken.verify(token);
+    const payload = await this.handlerToken.verify(token);
 
     if ((payload as IVerifyError).error) {
       return { code: code.UNAUTHORIZED, result: msgs.TOKEN_INVALID };
--- app/backend/src/utils/handlerToken/handlerToken.ts
+++ app/backend/src/utils/handlerToken/handlerToken.ts
@@ -4,23 +4,25 @@ import { Service } from 'typedi';
 import { IUser, IVerifyError } from '../../interfaces';
 
 @Service()
-export default class HandlerToken {
-  static readonly secret: Promise<string> = readFile('jwt.evaluation.key', 'utf8');
+class HandlerToken {
+  constructor(
+    private _secret: Promise<string> = readFile('jwt.evaluation.key', 'utf8'),
+  ) {}
 
-  static generate = async (user: IUser) => {
+  generate = async (user: IUser) => {
     const { id, email, username, role } = user;
     const payload = { id, email, username, role };
-    return jwt.sign(payload, (await HandlerToken.secret).trim(), { expiresIn: '7d' });
+    return jwt.sign(payload, (await this._secret).trim(), { expiresIn: '7d' });
   };
 
-  static verify = async (token: string) => {
+  verify = async (token: string) => {
     let result: unknown;
-
-    jwt.verify(token, (await HandlerToken.secret).trim(), (error, decoded) => {
-      const value = decoded as IUser;
-      result = { error, ...value } as typeof result;
+    jwt.verify(token, (await this._secret).trim(), (error, decoded) => {
+      result = { error, ...decoded };
     });
 
     return result as IVerifyError | IUser;
   };
 }
+
+export default new HandlerToken();
