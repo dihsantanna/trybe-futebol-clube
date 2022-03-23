b1d2d9cdbe4a049c97de8dc2c7efe3e6e32148ca
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-03-23 19:55:17 -0300

    cria testes de filtro inProgress passado como query na rota GET /matchs?query

--- app/backend/src/tests/matchs.test.ts
+++ app/backend/src/tests/matchs.test.ts
@@ -44,6 +44,63 @@ describe('Testa o endpoint GET /matchs', () => {
 
   it('verifica se retorna array com todas as partidas', () => {
     expect(chaiHttpResponse.body).to.be.deep.equal(matchsResponseSuccess)
-  })
+  });
 
-})
+});
+
+describe('Testa o endpoint GET /matchs?inProgress=bool', () => {
+  describe('passado "inProgress=true"', () => {
+    before(async () => {
+      sinon
+        .stub(Matchs, 'findAll')
+        .resolves(matchsInProgressResponse as unknown as Matchs[]);
+
+      chaiHttpResponse = await getChaiHttpResponse(
+        'GET',
+        '/matchs?inProgress=true',
+        '',
+      );
+    });
+
+    after(() => {
+      (Matchs.findAll as sinon.SinonStub).restore();
+    })
+
+    it('verifica se retorna status code 200', () => {
+      expect(chaiHttpResponse).to.have.status(code.OK);
+    });
+
+    it('verifica se retorna um array com as partidas em progresso', () => {
+      expect(chaiHttpResponse.body).to.be.deep.equal(matchsInProgressResponse);
+    });
+
+  });
+
+  describe('passado "inProgress=false"', () => {
+    before(async () => {
+      sinon
+        .stub(Matchs, 'findAll')
+        .resolves(matchsFinishedResponse as unknown as Matchs[]);
+
+      chaiHttpResponse = await getChaiHttpResponse(
+        'GET',
+        '/matchs?inProgress=false',
+        '',
+      );
+    });
+
+    after(() => {
+      (Matchs.findAll as sinon.SinonStub).restore();
+    })
+
+    it('verifica se retorna status code 200', () => {
+      expect(chaiHttpResponse).to.have.status(code.OK);
+    });
+
+    it('verifica se retorna um array com as partidas em progresso', () => {
+      expect(chaiHttpResponse.body).to.be.deep.equal(matchsFinishedResponse);
+    });
+
+  });
+
+});
