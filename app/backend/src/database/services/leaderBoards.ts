e4d225ff5e150647dbb2980706095d4f88195f9f
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-03-28 18:13:17 -0300

    refatorar leaderboard service

--- app/backend/src/database/services/leaderBoards.ts
+++ app/backend/src/database/services/leaderBoards.ts
@@ -72,44 +72,76 @@ export default class LeaderBoardsService {
     (points / (games * 3)) * 100
   ).toFixed(2);
 
-  readonly statisticsHomeTeam = (acc: IInfoTeams, curr: IMatchsAndClubNames) => {
+  readonly statisticsHomeTeam = (id: number, clubName: string, matchs: IMatchsAndClubNames[]) => {
     const { calcPoints, efficiency } = this;
-    const points = calcPoints(curr.homeTeamGoals, curr.awayTeamGoals);
-    acc.totalPoints += points;
-    acc.totalGames += 1;
-    acc.totalVictories += points === 3 ? 1 : 0;
-    acc.totalDraws += points === 1 ? 1 : 0;
-    acc.totalLosses += points === 0 ? 1 : 0;
-    acc.goalsFavor += curr.homeTeamGoals;
-    acc.goalsOwn += curr.awayTeamGoals;
-    acc.goalsBalance = acc.goalsFavor - acc.goalsOwn;
-    acc.efficiency = efficiency(acc.totalPoints, acc.totalGames);
+    return matchs.reduce((acc, curr) => {
+      if (curr.homeTeam === id) {
+        const points = calcPoints(curr.homeTeamGoals, curr.awayTeamGoals);
+        acc.name = clubName;
+        acc.totalPoints += points;
+        acc.totalGames += 1;
+        acc.totalVictories += points === 3 ? 1 : 0;
+        acc.totalDraws += points === 1 ? 1 : 0;
+        acc.totalLosses += points === 0 ? 1 : 0;
+        acc.goalsFavor += curr.homeTeamGoals;
+        acc.goalsOwn += curr.awayTeamGoals;
+        acc.goalsBalance = acc.goalsFavor - acc.goalsOwn;
+        acc.efficiency = efficiency(acc.totalPoints, acc.totalGames);
+        return acc;
+      }
+
+      return acc;
+    }, { ...initialInfos }) as unknown as IInfoTeams;
   };
 
-  readonly statisticsAwayTeam = (acc: IInfoTeams, curr: IMatchsAndClubNames) => {
+  readonly statisticsAwayTeam = (id: number, clubName: string, matchs: IMatchsAndClubNames[]) => {
     const { calcPoints, efficiency } = this;
-    const points = calcPoints(curr.awayTeamGoals, curr.homeTeamGoals);
-    acc.totalPoints += points;
-    acc.totalGames += 1;
-    acc.totalVictories += points === 3 ? 1 : 0;
-    acc.totalDraws += points === 1 ? 1 : 0;
-    acc.totalLosses += points === 0 ? 1 : 0;
-    acc.goalsFavor += curr.awayTeamGoals;
-    acc.goalsOwn += curr.homeTeamGoals;
-    acc.goalsBalance = acc.goalsFavor - acc.goalsOwn;
-    acc.efficiency = efficiency(acc.totalPoints, acc.totalGames);
+    return matchs.reduce((acc, curr) => {
+      if (curr.awayTeam === id) {
+        const points = calcPoints(curr.awayTeamGoals, curr.homeTeamGoals);
+        acc.name = clubName;
+        acc.totalPoints += points;
+        acc.totalGames += 1;
+        acc.totalVictories += points === 3 ? 1 : 0;
+        acc.totalDraws += points === 1 ? 1 : 0;
+        acc.totalLosses += points === 0 ? 1 : 0;
+        acc.goalsFavor += curr.awayTeamGoals;
+        acc.goalsOwn += curr.homeTeamGoals;
+        acc.goalsBalance = acc.goalsFavor - acc.goalsOwn;
+        acc.efficiency = efficiency(acc.totalPoints, acc.totalGames);
+        return acc;
+      }
+      return acc;
+    }, { ...initialInfos }) as unknown as IInfoTeams;
+  };
+
+  readonly statisticsAllTeams = (id: number, clubName: string, matchs: IMatchsAndClubNames[]) => {
+    const { statisticsHomeTeam, statisticsAwayTeam, efficiency } = this;
+    const home = statisticsHomeTeam(id, clubName, matchs);
+    const away = statisticsAwayTeam(id, clubName, matchs);
+    const all = {
+      name: clubName,
+      totalPoints: home.totalPoints + away.totalPoints,
+      totalGames: home.totalGames + away.totalGames,
+      totalVictories: home.totalVictories + away.totalVictories,
+      totalDraws: home.totalDraws + away.totalDraws,
+      totalLosses: home.totalLosses + away.totalLosses,
+      goalsFavor: home.goalsFavor + away.goalsFavor,
+      goalsOwn: home.goalsOwn + away.goalsOwn,
+      goalsBalance: home.goalsBalance + away.goalsBalance,
+      efficiency: 0,
+    };
+    all.efficiency = efficiency(all.totalPoints, all.totalGames);
+    return all as unknown as IInfoTeams;
   };
 
   leaderBoard = async () => {
     const { matchsRepository } = this;
-    const { clubsRepository, statisticsHomeTeam, statisticsAwayTeam, sortRules } = this;
+    const { clubsRepository, statisticsAllTeams, sortRules } = this;
     const matchs = await matchsRepository.findAll() as unknown as IMatchsAndClubNames[];
     const clubs = await clubsRepository.findAll();
-    const result = clubs.map(({ id, clubName }) => matchs.reduce((acc, curr) => {
-      if (curr.homeTeam === id) statisticsHomeTeam(acc, curr);
-      if (curr.awayTeam === id) statisticsAwayTeam(acc, curr);
-      return acc;
-    }, { ...initialInfos, name: clubName }));
+    const result = await Promise.all(clubs
+      .map(({ id, clubName }) => statisticsAllTeams(id, clubName, matchs)));
 
     result.sort(sortRules);
 
@@ -119,14 +151,10 @@ export default class LeaderBoardsService {
   homeTeamsRank = async () => {
     const { matchsRepository } = this;
     const { clubsRepository, statisticsHomeTeam, sortRules } = this;
-    const [matchs, clubs] = await Promise
-      .all([matchsRepository.findAll() as unknown as IMatchsAndClubNames[],
-        clubsRepository.findAll()]);
-    const result = clubs.map(({ id, clubName }) => matchs.reduce((acc, curr) => {
-      if (curr.homeTeam === id) statisticsHomeTeam(acc, curr);
-      acc.name = clubName;
-      return acc;
-    }, { ...initialInfos, name: clubName }));
+    const matchs = await matchsRepository.findAll() as unknown as IMatchsAndClubNames[];
+    const clubs = await clubsRepository.findAll();
+    const result = await Promise.all(clubs
+      .map(({ id, clubName }) => statisticsHomeTeam(id, clubName, matchs)));
 
     result.sort(sortRules);
 
@@ -138,11 +166,8 @@ export default class LeaderBoardsService {
     const { clubsRepository, statisticsAwayTeam, sortRules } = this;
     const matchs = await matchsRepository.findAll() as unknown as IMatchsAndClubNames[];
     const clubs = await clubsRepository.findAll();
-    const result = clubs.map(({ id, clubName }) => matchs.reduce((acc, curr) => {
-      if (curr.awayTeam === id) statisticsAwayTeam(acc, curr);
-      acc.name = clubName;
-      return acc;
-    }, { ...initialInfos, name: clubName }));
+    const result = await Promise.all(clubs
+      .map(({ id, clubName }) => statisticsAwayTeam(id, clubName, matchs)));
 
     result.sort(sortRules);
 
